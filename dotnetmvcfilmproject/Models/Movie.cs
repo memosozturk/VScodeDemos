3f7a6508fdf162c654d6b0a6cf9090d491e61b32
memosozturk <mozturk357@gmail.com>
2020-10-11 22:07:31 +0300

    a

---
+++ dotnetmvcfilmproject/Models/Movie.cs
@@ -0,0 +1,11 @@
+namespace dotnetmvcfilmproject.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Imageurl { get; set; }
+    }
+}
+//videoda 50.dakika 39.saniyede kaldım 22:07 11 ekim 
--- dotnetmvcfilmproject/Startup.cs
+++ dotnetmvcfilmproject/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace dotnetmvcfilmproject
 {
@@ -23,6 +25,11 @@ namespace dotnetmvcfilmproject
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();//wwwroot
+            app.UseStaticFiles(new StaticFileOptions{
+                FileProvider=new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"node_modules")),
+                RequestPath="/modules"
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
