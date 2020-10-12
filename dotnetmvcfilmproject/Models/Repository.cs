0ca9bc1eb27f3d7f712c0a52febba0989a6977bf
memosozturk <mozturk357@gmail.com>
2020-10-12 12:12:48 +0300

    ptesi

--- dotnetmvcfilmproject/Models/Repository.cs
+++ dotnetmvcfilmproject/Models/Repository.cs
@@ -13,7 +13,7 @@ namespace dotnetmvcfilmproject.Models
                 new Movie(){Id=2,Name="Hızlı ve Öfkeli 12",Description="Sonsuzluk",Imageurl="2.jpg"},
                 new Movie(){Id=3,Name="Avatar",Description="İkincisi gelecek diye 10 senedir bekliyoruz.",Imageurl="3.jpg"},
                 new Movie(){Id=4,Name="Yenilmezler",Description="Yaşasın Thanos Baba",Imageurl="4.jpg"},
-                new Movie(){Id=5,Name="Not Defteri",Description="Aşk,Ormantik",Imageurl="5.jpg"}
+                new Movie(){Id=5,Name="Instellar",Description="Uzay,Kara delik",Imageurl="5.jpg"}
             };
         }
         public static List<Movie> Movies{
